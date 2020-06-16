b4a4a22f5138c19675d11fece7ce70837b1d7149
Ahmed <ahmed2abd0l6ziz@gmail.com>
2020-06-16 23:46:37 +0200

    fixed bugs and added nested groups of Routes

--- public/index.php
+++ public/index.php
@@ -109,5 +109,28 @@ $app->group('/testgroup', function($group){
 		return $response;
 	});
 });
+// nested groups of Routes
+$app->group('/API', function($group){
+	$group->group('/v1', function($group){
+		$group->get('/getuser', function($request,$response){
+			$response->getBody()->write("get user v1");
+			return $response;
+		});
+		$group->post('/adduser', function($request,$response){
+			$response->getBody()->write("add user v1");
+			return $response;
+		});
+	});
+	$group->group('/v2', function($group){
+		$group->get('/getuser', function($request,$response){
+			$response->getBody()->write("get user v2");
+			return $response;
+		});
+		$group->post('/adduser', function($request,$response){
+			$response->getBody()->write("add user v2");
+			return $response;
+		});
+	});
+});
 // this is to run the whole code
 $app->run();
